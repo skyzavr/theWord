557c7c0d3ef1c79449d6b31ee670f036546ceacc
nino <dankozero111@gmail.com>
2024-01-11 21:45:59 +0300

    [fix]: phonetics & type in WordArea component

--- src/widgets/wordArea/ui/WordArea.tsx
+++ src/widgets/wordArea/ui/WordArea.tsx
@@ -1,17 +1,29 @@
 import AudioButton from '@shared/ui/audioButton/AudioButton';
 
+import { Data } from '@app/params';
+
 import classes from './wordArea.module.css';
 
-type Props = { word: string; transcription: string; audioHref: string };
+type Props = {
+  word: string;
+  params: Data[];
+};
 
-export const WordArea = ({ word, transcription, audioHref }: Props) => {
+export const WordArea = ({ word, params }: Props) => {
+  const { phonetics } = params[0];
+  const audioInd = phonetics.findIndex((el) => el.audio !== '');
+
   return (
     <div className={classes.wrapper}>
       <div className={classes.wordSection}>
         <div className={classes.word}>{word}</div>
-        <div className={classes.transcription}>{transcription}</div>
+        <div className={classes.transcription}>
+          {phonetics.map((el, ind) => (
+            <div key={ind}>{el.text}</div>
+          ))}
+        </div>
       </div>
-      <AudioButton src={audioHref} />
+      {audioInd >= 0 && <AudioButton src={phonetics[audioInd].audio} />}
     </div>
   );
 };
