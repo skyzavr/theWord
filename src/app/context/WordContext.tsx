4cc31778988ccf16e858200ec06cbb90d4f6058a
nino <dankozero111@gmail.com>
2024-01-11 21:54:27 +0300

    [add]: word context

---
+++ src/app/context/WordContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, useState, ReactNode, FC } from 'react';
+
+export type WordContextType = {
+  word: string;
+  setWord: (word: string) => void;
+};
+
+type Params = {
+  children: ReactNode;
+  init: string;
+};
+
+export const WordContext = createContext<WordContextType | null>(null);
+
+const WordContextProvider: FC<Params> = ({ children, init }) => {
+  const [word, setWord] = useState(init);
+  return (
+    <WordContext.Provider value={{ word, setWord }}>
+      {children}
+    </WordContext.Provider>
+  );
+};
+export default WordContextProvider;
