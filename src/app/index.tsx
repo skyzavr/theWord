ec4c94f815d512a1155844393300069c6459de2c
nino <dankozero111@gmail.com>
2024-01-11 21:55:09 +0300

    [add]:app layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <></>;
-}
+import ThemeContextProvider from './context/ThemeContext';
+import WordContextProvider from './context/WordContext';
+
+import { Layout } from '@widgets/layout';
+
+const App = () => {
+  return (
+    <ThemeContextProvider init="dark">
+      <WordContextProvider init="">
+        <Layout />
+      </WordContextProvider>
+    </ThemeContextProvider>
+  );
+};
 
 export default App;
